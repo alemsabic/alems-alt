b440f21e45415543baa2fb2e409893ed25458676
Alem <alemsabic@gmail.com>
2021-03-07 16:29:36 +0100

    Update theme.ts

--- .codedoc/theme.ts
+++ .codedoc/theme.ts
@@ -3,12 +3,12 @@ import { createTheme } from '@codedoc/core/transport';
 
 export const theme = /*#__PURE__*/createTheme({
   light: {
-    background: '#C8C8C8',           // --> CSS color string, background of the page
-    text: '#191970',            // --> CSS color string, color of text
-    primary: '#191970',             // --> CSS color string, primary color (for buttons, links, etc)
-    primaryContrast: '#C8C8C8',      // --> CSS color string, text color on primary background (e.g. buttons)
-    border: 'rgba(25,25,102,0.2)',               // --> CSS color string, border colors
-    code: '#191970',                  // --> CSS color string, in-text code color
+    background: '#ffffff',           // --> CSS color string, background of the page
+    text: '#000000',            // --> CSS color string, color of text
+    primary: '#000000',             // --> CSS color string, primary color (for buttons, links, etc)
+    primaryContrast: '#ffffff',      // --> CSS color string, text color on primary background (e.g. buttons)
+    border: 'rgba(0,0,0,0.2)',               // --> CSS color string, border colors
+    code: '#000000',                  // --> CSS color string, in-text code color
   },
   dark: {
     background: '#000',           // --> CSS color string, background of the page
@@ -25,15 +25,15 @@ export const theme = /*#__PURE__*/createTheme({
       border: 'rgba(105,240,174,0.4)',
     },
     light: {
-      background: '#C8C8C8',
-      text: '#191970',
-      border: 'rgba(25,25,102,0.2)',
+      background: '#ffffff',
+      text: '#000000',
+      border: 'rgba(0,0,0,0.2)',
     },
   },
   toc: {                                 
     light: {
-      background: 'rgba(200,200,200,0.4)',             // --> CSS color string, background of ToC
-      border: 'rgba(25,25,102,0.2)',                 // --> CSS color string, border of ToC
+      background: 'rgba(255,255,255,0.4)',             // --> CSS color string, background of ToC
+      border: 'rgba(0,0,0,0.2)',                 // --> CSS color string, border of ToC
     },
     dark: {
       background: 'rgba(0,0,0,0.4)',             // --> CSS color string, background of ToC
