2e87a62dee428be0363e931edc1cda3a3931784f
Alem <alemsabic@gmail.com>
2021-03-08 14:12:55 +0100

    Update theme.ts

--- .codedoc/theme.ts
+++ .codedoc/theme.ts
@@ -3,10 +3,10 @@ import { createTheme } from '@codedoc/core/transport';
 
 export const theme = /*#__PURE__*/createTheme({
   light: {
-    background: '#ffffff',           // --> CSS color string, background of the page
+    background: '#f0f2f2',           // --> CSS color string, background of the page
     text: '#000000',            // --> CSS color string, color of text
     primary: '#000000',             // --> CSS color string, primary color (for buttons, links, etc)
-    primaryContrast: '#ffffff',      // --> CSS color string, text color on primary background (e.g. buttons)
+    primaryContrast: '#f0f2f2',      // --> CSS color string, text color on primary background (e.g. buttons)
     border: 'rgba(0,0,0,0.2)',               // --> CSS color string, border colors
     code: '#000000',                  // --> CSS color string, in-text code color
   },
@@ -25,14 +25,14 @@ export const theme = /*#__PURE__*/createTheme({
       border: 'rgba(105,240,174,0.4)',
     },
     light: {
-      background: '#ffffff',
+      background: '#f0f2f2',
       text: '#000000',
       border: 'rgba(0,0,0,0.2)',
     },
   },
   toc: {                                 
     light: {
-      background: 'rgba(255,255,255,0.4)',             // --> CSS color string, background of ToC
+      background: 'rgba(240,242,242,0.4)',             // --> CSS color string, background of ToC
       border: 'rgba(0,0,0,0.2)',                 // --> CSS color string, border of ToC
     },
     dark: {
